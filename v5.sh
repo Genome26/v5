55e5c9a9c6236e8ae520766ab5b9140aeeac601a
Genome26 <njoroge.18125@students.kyu.ac.ke>
2024-08-22 18:41:53 +0300

    Update v5.sh

--- v5.sh
+++ v5.sh
@@ -656,11 +656,11 @@ wget -O /etc/systemd/system/ws.service "${REPO}Fls/ws.service" >/dev/null 2>&1
 chmod +x /etc/systemd/system/ws.service
 chmod +x /usr/bin/ws.py
 chmod 644 /usr/bin/tun.conf
-systemctl disable ws.py
-systemctl stop ws.py
-systemctl enable ws.py
-systemctl start ws.py
-systemctl restart ws.py
+systemctl disable ws
+systemctl stop ws
+systemctl enable ws
+systemctl start ws
+systemctl restart ws
 wget -q -O /usr/local/share/xray/geosite.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat" >/dev/null 2>&1
 wget -q -O /usr/local/share/xray/geoip.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat" >/dev/null 2>&1
 wget -O /usr/sbin/ftvpn "${REPO}Fls/ftvpn" >/dev/null 2>&1
