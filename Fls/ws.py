1d6acc6fc03bc79ea9ebfbc03b0478a4e49334d8
Genome26 <njoroge.18125@students.kyu.ac.ke>
2025-02-05 16:23:31 +0300

    Update ws.py

--- Fls/ws.py
+++ Fls/ws.py
@@ -3,7 +3,7 @@ import socket, threading, thread, select, signal, sys, time, getopt
 
 # Listen
 LISTENING_ADDR = '0.0.0.0'
-LISTENING_PORT = sys.argv[1]
+LISTENING_PORT = 10015
 
 # Pass
 PASS = ''
@@ -11,7 +11,7 @@ PASS = ''
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:109'
+DEFAULT_HOST = '127.0.0.1:143'
 RESPONSE = 'HTTP/1.1 101 <b><i><font color="green">WELCOME TO NETWORK TWEAKER</font></b>\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Accept: foo\r\n\r\n'
 
 class Server(threading.Thread):
